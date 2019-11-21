519afd51912baa72a9c07a145e0306035cc47b8c
kkBill <mjy332528@163.com>
2019-11-21 12:54:09 +0800

    2019-11-21 done

---
+++ src/LeetCode/_22_GenerateParentheses.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _22_GenerateParentheses {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack("",0,0,n,result);
+        return result;
+    }
+
+    // openCnt 是当前组合中"("的个数
+    // closeCnt 是当前组合中")"的个数
+    private void backtrack(String combination, int openCnt, int closeCnt, int n, List<String> result){
+        if(combination.length() == 2*n){
+            result.add(combination);
+            return;
+        }
+        if(closeCnt < openCnt) // 理解这一条件是本题重点
+            backtrack(combination+")",openCnt,closeCnt+1,n,result);
+        if(openCnt < n)
+            backtrack(combination+"(",openCnt+1,closeCnt,n,result);
+    }
+
+    public static void main(String[] args) {
+        _22_GenerateParentheses obj = new _22_GenerateParentheses();
+        System.out.println(obj.generateParenthesis(3));
+    }
+}
