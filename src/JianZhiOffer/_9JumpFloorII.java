23c852ef6fd49163bd4e179feccff37cc28dabea
kkBill <mjy332528@163.com>
2020-11-21 17:22:53 +0800

    remove JianZhiOffe

--- src/JianZhiOffer/_9JumpFloorII.java
+++
@@ -1,8 +0,0 @@
-package JianZhiOffer;
-
-public class _9JumpFloorII {
-    public int JumpFloorII(int target) {
-        //return (int)Math.pow(2,target-1);
-        return 1 << --target; // 利用位运算求2的n次幂，自己没想到！
-    }
-}
